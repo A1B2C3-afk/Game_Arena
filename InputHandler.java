68d2f854ce8af8a995313177ddb1d63dc2c1fccb
A1B2C3-afk <seifamr555555@gmail.com>
2025-12-18 12:50:07 +0200

    input handler

---
+++ InputHandler.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import java.util.HashSet;
+import java.util.Set;
+
+public class InputHandle
+    private Set<KeyCode> activeKeys = new HashSet<>();
+
+    public InputHandler(Scene scene) {
+
+        scene.setOnKeyPressed(event -> {
+            activeKeys.add(event.getCode());
+        });
+
+
+        scene.setOnKeyReleased(event -> {
+            activeKeys.remove(event.getCode());
+        });
+    }
+
+
+    public boolean isKeyPressed(KeyCode key) {
+        return activeKeys.contains(key);
+    }
+    
+    public void clear() {
+        activeKeys.clear();
+    }
+}
